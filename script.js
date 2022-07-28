0e95f8653ff780647c891c08d060e3816581519a
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-07-28 17:48:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+function preloadImage(url)
+{
+    var img=new Image();
+    img.src=url;
+}
+
+for(let i=0; i<30; i++){
+    preloadImage('./img/'+i+'.jpg')
+}
+
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let currentTime = 60
+let hitPosition 
+let timerId = null
+
+function randomSquare(){
+    squares.forEach(square =>{
+        square.classList.remove('mole')
+        square.style.backgroundImage='none'
+    })
+
+    let randomSquare = squares[Math.floor(Math.random()*9)]
+    randomSquare.classList.add('mole')
+    let rnd = Math.floor(Math.random()*28)+1
+    let urlstr = 'url(./img/'+rnd+'.jpg)'
+    randomSquare.style.backgroundImage = urlstr
+
+    hitPosition=randomSquare.id
+}
+
+squares.forEach(square=>{
+    square.addEventListener('mousedown', ()=>{
+        if(square.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole(){
+    timerId = setInterval(randomSquare,1000)
+}
+
+moveMole()
+
+let countDowntimerId = setInterval(countDown,1000)
+
+function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime<0){
+        clearInterval(countDowntimerId)
+        clearInterval(timerId)
+        timeLeft.textContent = '0'
+        alert('Игра закончена! Ты поймал ' + result +' Гриш из 60 возможных!')
+    }
+}
+
